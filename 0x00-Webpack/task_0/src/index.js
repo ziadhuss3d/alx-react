b6ac8de7d7e0f222dfa9326c2b0cbabed6025113
ziadhuss3d <ziadhuss3d@example.com>
2024-11-01 20:57:50 +0000

    web

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
+
