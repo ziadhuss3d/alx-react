3da742403dd330e5cc3f1b0e6d4bf51d25f3f07b
ziadhuss3d <ziadhuss3d@example.com>
2024-11-22 01:41:54 +0000

    n

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,7 @@
+
+import { fromJS } from 'immutable';
+
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
+
---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,9 @@
+
+import { fromJS } from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
+}
+
